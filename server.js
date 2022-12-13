1f1bdba493504fa0431ba1ce261fd41b23161689
sDUDEnt <ia04kuchynskyiboryslav@gmail.com>
2022-12-13 22:08:39 +0200

    testing actions

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 
-// константы
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// приложение
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('what a great view! <img src="https://c.files.bbci.co.uk/64FB/production/_127015852_mediaitem127015851.jpg">');
+  res.send('what a great view!!!! <img src="https://c.files.bbci.co.uk/64FB/production/_127015852_mediaitem127015851.jpg">');
 });
 
 app.listen(PORT, HOST, () => {
